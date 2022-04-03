e04693204e63a0c83f884f9fc04d2173d12deb7a
sayanpal469 <sayanpal469@gmail.com>
2022-04-04 01:59:28 +0530

    not-found added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home'
 import Reviews from './components/Reviews/Reviews';
 import Dashboard from './components/Dashboard/Dashboard'
 import Blogs from './components/Blogs/Blogs'
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/reviews' element={ <Reviews/> }></Route>
         <Route path='/dashboard' element={ <Dashboard/> }></Route>
         <Route path='/blogs' element={ <Blogs/> }></Route>
+        <Route path='*' element={ <NotFound/> }></Route>
       </Routes>
     </div>
   );
